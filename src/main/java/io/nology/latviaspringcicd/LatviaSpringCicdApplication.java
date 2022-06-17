2163d6d90603e1ec792c5127b81b63c5c7f9b29f
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-06-17 11:21:02 +1000

    prettier formatting

--- src/main/java/io/nology/latviaspringcicd/LatviaSpringCicdApplication.java
+++ src/main/java/io/nology/latviaspringcicd/LatviaSpringCicdApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LatviaSpringCicdApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LatviaSpringCicdApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(LatviaSpringCicdApplication.class, args);
+  }
 }
