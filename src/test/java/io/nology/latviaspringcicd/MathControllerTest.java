79ab007bb0b877858c45a3560f8f5a2950f16588
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-06-17 11:11:17 +1000

    fix indentation

--- src/test/java/io/nology/latviaspringcicd/MathControllerTest.java
+++ src/test/java/io/nology/latviaspringcicd/MathControllerTest.java
@@ -9,32 +9,31 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class MathControllerTest {
-    @Autowired
-    private MathController controller;
-    @Test
-    public void rectanglePerimeterShouldReturnCorrectPerimeter() {
-        Integer height = 100;
-        Integer width = 200; 
-        Integer result  = controller.rectanglePerimeter(height, width);
-        Integer expected = 600;
-        assertEquals(expected, result);
-    }
-    
-    @Test
-    public void rectanglePerimeterShouldThrowWhenArgNull() {
+  @Autowired
+  private MathController controller;
+  @Test
+  public void rectanglePerimeterShouldReturnCorrectPerimeter() {
       Integer height = 100;
-      Integer width =  null;
-      assertThrows(NullPointerException.class, () -> {
-        controller.rectanglePerimeter(height, width);
-      });
+      Integer width = 200; 
+      Integer result  = controller.rectanglePerimeter(height, width);
+      Integer expected = 600;
+      assertEquals(expected, result);
     }
     
     @Test
-    public void circleAreaShouldReturnCorrectArea() {
-      Double r = 2d;
-      Double result = controller.circleArea(r);
-      assertEquals(12.56, result);
-        
+  public void rectanglePerimeterShouldThrowWhenArgNull() {
+    Integer height = 100;
+    Integer width =  null;
+    assertThrows(NullPointerException.class, () -> {
+      controller.rectanglePerimeter(height, width);
+    });
+    }
+    
+    @Test
+  public void circleAreaShouldReturnCorrectArea() {
+    Double r = 2d;
+    Double result = controller.circleArea(r);
+    assertEquals(12.56, result); 
     }
 
 }
